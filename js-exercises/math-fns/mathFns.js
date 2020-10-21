1dc97c0c2cc4ae811a24971e44eef78146831bcd
Vishal Tom <vishal.tom995@gmail.com>
2020-10-22 01:44:21 +0530

    solutions-math-fns

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,12 @@
+// You can use the function from the `Math` module.
+function sqrt(args) {
+  return Math.sqrt(args);
+}
+function power(x, y) {
+  return x ** y;
+}
+function round(args) {
+  return Math.round(args);
+}
+// Don't change the exported names.
+export { sqrt, power, round };
